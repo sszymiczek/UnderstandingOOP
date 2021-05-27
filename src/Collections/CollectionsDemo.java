54959a20c3e2d0c9f500a586f68bef20d518d778
Szymik <szymonszymiczek32@gmail.com>
2021-05-27 23:47:32 +0200

    Comparator

---
+++ src/Collections/CollectionsDemo.java
@@ -0,0 +1,13 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class CollectionsDemo {
+    public static void show() {
+        Collection<String> collection = new ArrayList<>();
+
+
+    }
+}
--- src/Collections/Main.java
+++ src/Collections/Main.java
@@ -2,12 +2,17 @@ package Collections;
 
 import Generics.GenericList;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        var list = new GenericList<String>();
-        list.add("w");
-        list.add("e");
-        for (var item : list)
-            System.out.println(item);
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("b", "e3"));
+        customers.add(new Customer("a", "e2"));
+        customers.add(new Customer("c", "e1"));
+        Collections.sort(customers, new EmailComparator());
+        System.out.println(customers);
     }
 }
